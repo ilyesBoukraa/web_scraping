e8f1857cf189f17b3bbcfee3ce70e516f1d72ff2
ilyesBoukraa <boukraailyesali@gmail.com>
2023-07-12 14:46:35 +0200

    Update displaying_csv.py

--- displaying_csv.py
+++ displaying_csv.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 if __name__=="__main__":
     df = pd.read_csv('output.csv')
+    # I did not need to use n=5
+    # because it is the defualt params
+    # however I always forget the attributes
+    # of the head() method... (I should make it as an argument using CLI)
     print(df.head(n=5))
